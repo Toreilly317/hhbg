92b3f86068d383c6420eff43e98f9062b8513d44
Toreilly317 <Taylor@TaylorOReilly.com>
2018-08-20 14:11:58 -0400

    add registration and login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://TObeats:AdminBreaker3!@ds127342.mlab.com:27342/hhbg"
+  mongoURI: "mongodb://TObeats:AdminBreaker3!@ds127342.mlab.com:27342/hhbg",
+  secretOrKey:
+    "0ogG3vmqWcEr9XuYbwaW8GkhG2TFd1TUD7DbMkNpWD5CO5rm4FyQPdgFvnB3GyuHq57tn6rheEO0AFMjV66iNbfUAwGpGNIpyvvCnA83sAyG7MHTJGHTueAmUYmSBbiGnIbJvb6gd5CoPA4cFVKZ7spZ2KHdGV1incG84eUqgQshcpGoTezhpDVgPmh3V7VRNc6J8cbW"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secretOrKey = require("../../config/keys").secretOrKey;
 
 //load user model
 const User = require("../../models/User");
@@ -33,5 +35,36 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+//@route POST api/users/login
+//@desc login user / return jwt
+//@access Public
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+    //check pw
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //user matched
+
+        //create jwt payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        //sign token
+        jwt.sign(payload, secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            sucess: true,
+            token: `Bearer ${token}`,
+            err
+          });
+        });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
   });
 });
